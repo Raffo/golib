fb3fe3ecb95c29fcd06bfc1f16e16f80c0a71c3c
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2022-10-22 17:47:24 +0200

    make it sane
    Signed-off-by: Raffaele Di Fazio <difazio.raffaele@gmail.com>

--- pkg/cmd/cmdexec.go
+++ pkg/cmd/cmdexec.go
@@ -1,31 +1,21 @@
 package cmd
 
 import (
-	"bufio"
-	"fmt"
-	"io"
+	"log"
 	"os/exec"
 	"strings"
 )
 
 func CmdExec(command string, panicOnError bool) ([]byte, error) {
-	fmt.Printf("running %s\n", command)
+	log.Printf("running %s\n", command)
 	commandAndArgs := strings.Split(command, " ")
 	cmd := exec.Command(commandAndArgs[0], commandAndArgs[1:]...)
-
-	outReader, _ := cmd.StdoutPipe()
-	errReader, _ := cmd.StderrPipe()
-
-	cmd.Start()
-
-	cmdReader := io.MultiReader(outReader, errReader)
-
-	reader := bufio.NewReader(cmdReader)
-	line, err := reader.ReadString('\n')
-	for err == nil {
-		fmt.Println(line)
-		line, err = reader.ReadString('\n')
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		if panicOnError {
+			log.Fatal(err) //let's call this "set -e"
+		}
+		return out, err
 	}
-	cmd.Wait()
-	return nil, nil
+	return out, nil
 }
