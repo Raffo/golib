5b5c3cc1485ff7df337607924231ad3324135ec1
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2024-03-02 12:28:41 +0100

    fix empty dir
    Signed-off-by: Raffaele Di Fazio <difazio.raffaele@gmail.com>

--- pkg/cmd/cmdexec.go
+++ pkg/cmd/cmdexec.go
@@ -10,7 +10,9 @@ func CmdExec(command, dir string, panicOnError bool) ([]byte, error) {
 	log.Printf("running %s\n", command)
 	commandAndArgs := strings.Split(command, " ")
 	cmd := exec.Command(commandAndArgs[0], commandAndArgs[1:]...)
-	cmd.Dir = dir
+	if dir != "" {
+		cmd.Dir = dir
+	}
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		if panicOnError {
