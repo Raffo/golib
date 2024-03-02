716fb021b8ddb09b49bc7b4032719c5cf9ec4937
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2024-03-02 12:25:29 +0100

    add dir to CmdExec
    Signed-off-by: Raffaele Di Fazio <difazio.raffaele@gmail.com>

--- pkg/cmd/cmdexec.go
+++ pkg/cmd/cmdexec.go
@@ -6,10 +6,11 @@ import (
 	"strings"
 )
 
-func CmdExec(command string, panicOnError bool) ([]byte, error) {
+func CmdExec(command, dir string, panicOnError bool) ([]byte, error) {
 	log.Printf("running %s\n", command)
 	commandAndArgs := strings.Split(command, " ")
 	cmd := exec.Command(commandAndArgs[0], commandAndArgs[1:]...)
+	cmd.Dir = dir
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		if panicOnError {
